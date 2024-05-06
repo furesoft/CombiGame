4bf0fc0034961f5307ceac55f084dac7b2d49106
Chris <filmee24@gmail.com>
2024-05-06 15:19:44 +0200

    Update ItemDefinitions.cs

--- CombiGame/Models/ItemDefinitions.cs
+++ CombiGame/Models/ItemDefinitions.cs
@@ -2,18 +2,18 @@ namespace CombiGame.Models;
 
 public class ItemDefinitions {
     public static Item PLANT = new(nameof(PLANT));
-    public static  Item WATER = new(nameof(WATER));
-    public static  Item SUN = new(nameof(SUN));
-    public static  Item SAND = new(nameof(SAND));
-    public static  Item STORM = new(nameof(STORM));
-    public static  Item HUMAN = new(nameof(HUMAN));
-    public static  Item BIRD = new(nameof(BIRD));
-    public static  Item PLANET = new(nameof(PLANET));
-    public static  Item SOIL = new(nameof(SOIL));
-    public static  Item FLOWER = new(nameof(FLOWER));
-    public static  Item TREE = new(nameof(TREE));
-    public static  Item FOREST = new(nameof(FOREST));
-    public static  Item LAKE = new(nameof(LAKE));
-    public static  Item OCEAN = new(nameof(OCEAN));
-    public static  Item STEAM = new(nameof(STEAM));
+    public static Item WATER = new(nameof(WATER));
+    public static Item SUN = new(nameof(SUN));
+    public static Item SAND = new(nameof(SAND));
+    public static Item STORM = new(nameof(STORM));
+    public static Item HUMAN = new(nameof(HUMAN));
+    public static Item BIRD = new(nameof(BIRD));
+    public static Item PLANET = new(nameof(PLANET));
+    public static Item SOIL = new(nameof(SOIL));
+    public static Item FLOWER = new(nameof(FLOWER));
+    public static Item TREE = new(nameof(TREE));
+    public static Item FOREST = new(nameof(FOREST));
+    public static Item LAKE = new(nameof(LAKE));
+    public static Item OCEAN = new(nameof(OCEAN));
+    public static Item STEAM = new(nameof(STEAM));
 }
